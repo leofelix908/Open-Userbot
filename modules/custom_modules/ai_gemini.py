a463b2a387f0d595bbd23890490ab2630e029d82
xzrci <itxmishii@gmail.com>
2024-12-09 18:55:58 +0700

    Update ai_gemini.py

--- modules/custom_modules/ai_gemini.py
+++ modules/custom_modules/ai_gemini.py
@@ -10,9 +10,9 @@ NEW_GEMINI_URL = (
 
 async def fetch_response(url: str, query: str, message: Message):
     response_msg = await (
-        message.edit("<code>Umm, lemme thinking...</code>")
+        message.edit("<code>Umm, lemme think...</code>")
         if message.from_user.is_self
-        else message.reply("<code>Umm, lemme thinking...</code>")
+        else message.reply("<code>Umm, lemme think...</code>")
     )
     try:
         response = requests.get(url)
